3a6449f5e8f3f50b4121193955351235c91f7bdf
l0rraine <idn.lee@gmail.com>
2018-02-26 15:27:51 +0800

    use ajax to get dialog and content

---
+++ src/app/Models/DdvueMenu.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace DDVue\AdminPanel\app\Models;
+
+use DDVue\Crud\app\Models\BaseClassifiedModel;
+use DDVue\Crud\ModelTraits\BaseModelTrait;
+use Illuminate\Support\Facades\Route;
+
+class DdvueMenu extends BaseClassifiedModel
+{
+    use BaseModelTrait;
+
+    protected $table = 'ddvue_menus';
+
+    public static function rules($id = 0, $merge = [])
+    {
+        return array_merge([
+            'title'     => 'required',
+            'parent_id' => 'required',
+        ], $merge);
+    }
+
+    public static function messages($id = 0, $merge = [])
+    {
+        return array_merge([
+            'title.required'     => '必须填写标题',
+            'parent_id.required' => '必须选择父节点！',
+        ], $merge);
+    }
+
+    public static function setIndexAuto()
+    {
+        foreach (self::all() as $k => $v) {
+            if ($v['index'] == '') {
+                $index = substr($v['class_list'], 1, strlen($v['class_list']) - 2);
+                $arr   = explode(',', $index);
+                $index = join('-', $arr);
+                $v->update(['index' => $index]);
+            }
+        }
+    }
+
+    public function extraActionWhenRecurse(array $arr)
+    {
+
+        if(Route::has($arr['index'])) $arr['index']=route($arr['index']);
+        return $arr;
+    }
+}
---
+++ src/database/migrations/2018_02_24_084138_create_ddvue_menus_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDdvueMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ddvue_menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('index')->nullable()->comment('类型为submenu,group时为空,为item时填写RouteName');
+            $table->string('type')->comment('类型：submenu,group,item');
+            $table->string('icon')->nullable();
+            $table->string('owners')->nullable()->comment('能够查看该节点的角色id，格式为,1,2,3,');
+            $table->integer('sort_id')->default(99);
+            $table->integer('parent_id');
+            $table->string('class_list')->nullable();
+            $table->string('class_layer')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ddvue_menus');
+    }
+}
