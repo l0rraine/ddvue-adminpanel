e96268d70d61fa017a9d4b44248b0ee80dc1341f
l0rraine <idn.lee@gmail.com>
2018-10-23 10:58:43 +0800

    minor fix

--- src/app/Models/DdvRole.php
+++ src/app/Models/DdvRole.php
@@ -33,11 +33,6 @@ class DdvRole extends Role
         );
     }
 
-    protected function ensureModelSharesGuard($roleOrPermission)
-    {
-        return true;
-    }
-
 //    public static function findByName(string $name, $guardName = null): RoleContract
 //    {
 //        $guardName = $guardName ?? Guard::getDefaultName(static::class);
