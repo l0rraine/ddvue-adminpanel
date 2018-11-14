759479e0e31b30d34dba1da001d6e3c7422dce26
l0rraine <idn.lee@gmail.com>
2018-11-14 10:48:03 +0800

    minor fix

--- src/database/seeds/DdvueRoleSeeder.php
+++ src/database/seeds/DdvueRoleSeeder.php
@@ -13,25 +13,27 @@ class DdvueRoleSeeder extends Seeder
      */
     public function run()
     {
+        $guard = \Spatie\Permission\Guard::getDefaultName(config('ddvue.adminpanel.auth.user_model'));
         app()['cache']->forget('spatie.permission.cache');
 
-        $guard = config('ddvue.adminpanel.auth.admin_auth_middleware');
-        if(count(explode(':',$guard))==1){
-            $guard = config('auth.defaults.guard');
-        }else{
-            $guard = explode(':',$guard)[1];
-        }
+
+//        $guard = config('ddvue.adminpanel.auth.admin_auth_middleware');
+//        if(count(explode(':',$guard))==1){
+//            $guard = config('auth.defaults.guard');
+//        }else{
+//            $guard = explode(':',$guard)[1];
+//        }
 
 
-        Permission::create(['name' => '编辑 后台菜单','guard_name'=>$guard]);
-        Permission::create(['name' => '编辑 权限角色','guard_name'=>$guard]);
+        \DDVue\AdminPanel\app\Models\DdvPermission::create(['name' => '编辑 后台菜单','guard_name'=>$guard]);
+        \DDVue\AdminPanel\app\Models\DdvPermission::create(['name' => '编辑 权限角色','guard_name'=>$guard]);
 
 
-        $role = Role::create(['name' => '超级管理员','guard_name'=>$guard]);
+        $role = \DDVue\AdminPanel\app\Models\DdvRole::create(['name' => '超级管理员','guard_name'=>$guard]);
         $role->givePermissionTo('编辑 后台菜单');
         $role->givePermissionTo('编辑 权限角色');
 
-        $role = Role::create(['name' => '管理员']);
+        $role = \DDVue\AdminPanel\app\Models\DdvRole::create(['name' => '管理员']);
 
         $user = \DDVue\AdminPanel\app\Models\DdvUser::where('email', 'liqiang150.slyt')->first();
         $user->assignRole('超级管理员');
